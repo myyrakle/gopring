7a8434aba0c6b5f64d072b6b0ad317028aebd2d5
myyrakle <sssang97@naver.com>
2023-08-15 08:56:19 +0900

    impl: ReplaceTemplate

---
+++ pkg/template/replacer.go
@@ -0,0 +1,12 @@
+package template
+
+import "strings"
+
+// {{name}} 형태의 템플릿을 주어진 변수로 치환합니다.
+func ReplaceTemplate(template string, templateVariables map[string]string) string {
+	for key, value := range templateVariables {
+		template = strings.ReplaceAll(template, "{{"+key+"}}", value)
+	}
+
+	return template
+}
