862a34d8d072177245a7fc885a449d8c611ef1d0
myyrakle <sssang97@naver.com>
2023-09-06 01:35:50 +0900

    impl: getMappingAnnotaion

--- internal/generator/file.go
+++ internal/generator/file.go
@@ -1,6 +1,7 @@
 package generator
 
 import (
+	"fmt"
 	"go/ast"
 )
 
@@ -10,6 +11,20 @@ func processFile(packageName string, filename string, file *ast.File, output *Ro
 	var controllerCodes []string
 
 	for _, declare := range file.Decls {
+		if fn, ok := declare.(*ast.FuncDecl); ok {
+			fmt.Println("@#@#", fn.Name, fn.Doc)
+			if fn.Recv != nil {
+				if len(fn.Recv.List) > 0 {
+					if ident, ok := fn.Recv.List[0].Type.(*ast.Ident); ok {
+						fmt.Println("@#@#", ident.Name)
+						// if ident.Name == "HomeController" {
+						// 	fmt.Printf("Receiver method: %s\n", fn.Name.Name)
+						// }
+					}
+				}
+			}
+		}
+
 		if genDecl, ok := declare.(*ast.GenDecl); ok {
 			for _, spec := range genDecl.Specs {
 				if typeSpec, ok := spec.(*ast.TypeSpec); ok {
