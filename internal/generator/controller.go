049a5892ab9261c490bfce1620110950eea7fb9b
myyrakle <sssang97@naver.com>
2023-09-11 00:03:37 +0900

    impl: controller alias generator

--- internal/generator/controller.go
+++ internal/generator/controller.go
@@ -1,7 +1,6 @@
 package generator
 
 import (
-	"fmt"
 	"go/ast"
 	"strings"
 
@@ -42,6 +41,7 @@ func processContoller(packageName string, annotaion annotation.Annotaion, struct
 	newFunctionName := "GopringNewController" + structName
 
 	output.Providers = append(output.Providers, packageName+"."+newFunctionName)
+	output.RunGopringParameters = append(output.RunGopringParameters, packageName+"."+structName)
 
 	newFunctionCode += "func " + newFunctionName + "("
 
@@ -60,7 +60,9 @@ func processContoller(packageName string, annotaion annotation.Annotaion, struct
 				packageName := ident.Name
 				typeName += packageName + "." + expr.Sel.Name
 
-				newFunctionCode += fieldName + " " + typeName + ", "
+				field := fieldName + " " + typeName
+
+				newFunctionCode += field + ", "
 				continue
 			}
 		}
@@ -68,12 +70,10 @@ func processContoller(packageName string, annotaion annotation.Annotaion, struct
 		if ident, ok := typeExpr.(*ast.Ident); ok {
 			typeName := ident.Name
 
-			fmt.Println(">> " + fieldName + " : " + typeName)
-			typeName += ident.Name
+			field := fieldName + " " + typeName
 
-			fmt.Println(">> " + fieldName + " : " + typeName)
-
-			newFunctionCode += fieldName + " " + typeName + ", "
+			newFunctionCode += field + ", "
+			continue
 		}
 	}
 
--- pkg/alias/package_alias.go
+++ pkg/alias/package_alias.go
@@ -2,18 +2,18 @@ package alias
 
 import "fmt"
 
-var count int = 0
+var pkgCount int = 0
 
 // package alias가 참조되는 개수입니다. (0일 경우 import가 되지 않도록 할때 응용합니다.)
 var PackageAliasRefCount map[string]int = make(map[string]int)
 
 func ResetPackageCount() {
-	count = 0
+	pkgCount = 0
 }
 
 func GetNextPackageAlias() string {
-	count++
-	alias := fmt.Sprintf("g%06d", count)
+	pkgCount++
+	alias := fmt.Sprintf("gp%06d", pkgCount)
 
 	PackageAliasRefCount[alias] = 0
 	return alias
