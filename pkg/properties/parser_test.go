46df58993cb981bc229761e1f88fadcdf71ea5ff
myyrakle <sssang97@naver.com>
2023-09-21 02:59:59 +0900

    [#32] fix: testcase

--- pkg/properties/parser_test.go
+++ pkg/properties/parser_test.go
@@ -1,15 +1,43 @@
 package properties
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 )
 
 func Test_ParseProperties(t *testing.T) {
-	rawString := `server.port = 8888
+	type testCase struct {
+		inputText string
+		want      Properties
+	}
+
+	testCases := []testCase{
+		{
+			inputText: `server.port = 8888
 spring.profiles = dev
 spring.application.name = demo
-`
-	properties := ParseProperties(rawString)
-	fmt.Printf("%+v\n", properties)
+`,
+			want: Properties{
+				Property{
+					Key:   "server.port",
+					Value: "8888",
+				},
+				Property{
+					Key:   "spring.profiles",
+					Value: "dev",
+				},
+				Property{
+					Key:   "spring.application.name",
+					Value: "demo",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		got := ParseProperties(tc.inputText)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("ParseProperties(%q) = %q; want %q", tc.inputText, got, tc.want)
+		}
+	}
 }
